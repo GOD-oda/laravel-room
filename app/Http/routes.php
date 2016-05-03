89c8e61f7f67af54031a006c098fa95e7ed9649b
Takahiro Oda <conpw3@gmail.com>
2016-05-04 07:56:51 +0900

    update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,6 +8,13 @@ Route::group(['middleware' => 'web'], function () {
         Route::controller('/', 'ArticlesController');
     });
 
+    // tool
+    Route::group(['domain' => 'tool.t-oda.tech'], function() {
+        Route::get('/', function() {
+            return 1;
+        });
+    });
+
     // portfolio
     Route::get('/', function() {
         return view('portfolio.index');
