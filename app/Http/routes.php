8d7e6304a0e61d1700eada7f78bdc8c1ffef8329
Takahiro Oda <conpw3@gmail.com>
2016-05-08 11:56:54 +0900

    サブドメインとルーティングの変更
    管理画面はまとめて、adminというサブドメインで行う。
    blogはBlogControllerが、支払い系はPaymentControllerが担当

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,19 +2,21 @@
 
 Route::group(['middleware' => 'web'], function () {
     // blog
-    Route::group(['domain' => 'blog.t-oda.tech'], function() {
+    /*Route::group(['domain' => 'blog.t-oda.tech'], function() {
         Route::auth();
         Route::resource('/admin', 'AdminController');
         Route::controller('/', 'ArticlesController');
+    });*/
+
+    // admin
+    Route::group(['dmain' => 'admin.t-oda.tech'], function() {
+        Route::auth();
+        Route::resource('/blog', 'BlogController');
+        Route::resource('/payment', 'PaymentController');
     });
 
-    // tool
-    Route::group(['domain' => 'tool.t-oda.tech'], function() {
-        Route::get('/', function() {
-            abort(503);
-            //return view('tool.index');
-        });
-    });
+    // blog
+    Route::controller('/', 'ArticlesController');
 
     // portfolio
     Route::get('/', function() {
--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -3,12 +3,15 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="このサイトは、日々奮闘する下っ端プログラマのOdaがオールジャンルの情報を提供し、運営するサイトです。">
   <meta name="author" content="ODA">
-  <link rel="shortcut icon" href="assets/img/favicon.png">
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
   <title>{{ Config::get('word.title') }}</title>
+
+  <link rel="shortcut icon" href="assets/img/favicon.png">
 
   <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
@@ -23,54 +26,43 @@
   <!-- Import Google Icon Font-->
   <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-  <!--Let browser know website is optimized for mobile-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
   <![endif]-->
 </head>
-
 <body class="blue-grey lighten-5">
-
-<header>
-  <nav class="teal">
-    <div class="nav-wrapper">
-      <div class="container">
-        <div class="brand-logo">{{ $logo }}</div>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="{{ url('admin') }}"><i class="material-icons left">list</i>一覧</a></li>
-          <li><a href="{{ url('admin/create') }}"><i class="material-icons left">create</i>記事作成</a></li>
-          <!-- <li><a href="{{ url('auth/register') }}">ユーザ登録</a></li> -->
-          <li><a href="{{ url('logout') }}"><i class="material-icons left">exit_to_app</i>ログアウト</a></li>
-        </ul>
+  <header>
+    <nav class="teal">
+      <div class="nav-wrapper">
+        <div class="container">
+          <div class="brand-logo">{{ $logo }}</div>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="{{ url('blog') }}"><i class="material-icons left">list</i>一覧</a></li>
+            <li><a href="{{ url('blog/create') }}"><i class="material-icons left">create</i>記事作成</a></li>
+            <!-- <li><a href="{{ url('auth/register') }}">ユーザ登録</a></li> -->
+            <li><a href="{{ url('logout') }}"><i class="material-icons left">exit_to_app</i>ログアウト</a></li>
+          </ul>
+        </div>
       </div>
+    </nav>
+  </header>
+  <main>
+    <div class="container">
+      @yield('content')
     </div>
-  </nav>
-</header>
+  </main>
 
+  <!-- Bootstrap core JavaScript -->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.5/marked.min.js"></script>
 
-<main>
-  <div class="container">
-    @yield('content')
-  </div>
-</main>
-
-
-<!-- Bootstrap core JavaScript -->
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/jquery-ui.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.5/marked.min.js"></script>
-
-<!-- Custome script for this template -->
-<script src="{{ asset('js/materialize.min.js') }}"></script>
-<script src="{{ asset('js/boot_materialize.js') }}"></script>
-<script src="{{ asset('js/boot_marked.js') }}"></script>
-
-
+  <!-- Custome script for this template -->
+  <script src="{{ asset('js/materialize.min.js') }}"></script>
+  <script src="{{ asset('js/boot_materialize.js') }}"></script>
+  <script src="{{ asset('js/boot_marked.js') }}"></script>
 </body>
-
 </html>
