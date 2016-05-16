23079fdd9929cf4b8dff8ce95694e9ef12f91c5a
oda <oda_takahiro@mut.co.jp>
2016-05-16 10:18:59 +0900

    フォントの修正

--- resources/views/layouts/home_master.blade.php
+++ resources/views/layouts/home_master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="{{ Config::get('word.meta.discription') }}">
   <meta name="author" content="ODA">
