827bc5177f430a15a0fce53a21ae12dc7b3376ac
Takahiro Oda <conpw3@gmail.com>
2016-05-22 13:35:56 +0900

    css修正とhoverable削除

--- resources/views/layouts/home_master.blade.php
+++ resources/views/layouts/home_master.blade.php
@@ -30,6 +30,8 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
   <![endif]-->
+
+  <!-- Google Analytics Tracking Code -->
   <script src="{{ asset('js/trackingcode.js') }}"></script>
 </head>
 <body class="blue-grey lighten-5">
