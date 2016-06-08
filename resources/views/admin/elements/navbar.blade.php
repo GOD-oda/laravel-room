f0d03a34cb77d8457e6c8f41fe62fae9468dec41
Takahiro Oda <conpw3@gmail.com>
2016-06-08 23:11:54 +0900

    管理画面のnavのリンク修正

--- resources/views/admin/elements/navbar.blade.php
+++ resources/views/admin/elements/navbar.blade.php
@@ -3,8 +3,8 @@
     <div class="container">
       <div class="brand-logo">{{ $logo }}</div>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="{{ url('blog') }}">BLOG</a></li>
-        <li><a href="{{ url('payment') }}">PAYMENT</a></li>
+        <li><a href="{{ route('admin.blog.index') }}">BLOG</a></li>
+        <li><a href="{{ route('admin.payment.index') }}">PAYMENT</a></li>
         <!-- <li><a href="{{ url('blog/create') }}"><i class="material-icons left">create</i>記事作成</a></li> -->
         <!-- <li><a href="{{ url('auth/register') }}">ユーザ登録</a></li> -->
         <li><a href="{{ url('logout') }}"><i class="material-icons left">exit_to_app</i>LOGOUT</a></li>
