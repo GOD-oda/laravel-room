741127c6dcdaff0cbf97ef116198642dec044e62
Takahiro Oda <conpw3@gmail.com>
2016-06-09 00:41:27 +0900

    記事詳細ページの体裁調整

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,9 +8,13 @@
     <div class="article-show">
       <h1>{{ $article->title }}</h1>
       <div class="divider"></div>
-      <div class="right mt5">
-        <i class="material-icons">build</i> {{ date('Y.m.d', strtotime($article->created_at)) }}
-        <i class="material-icons">autorenew</i> {{ date('Y.m.d', strtotime($article->updated_at)) }}
+      <div class="row">
+        <div class="col s12">
+          <div class="right mt5">
+            <i class="material-icons">build</i> {{ date('Y.m.d', strtotime($article->created_at)) }}
+            <i class="material-icons">autorenew</i> {{ date('Y.m.d', strtotime($article->updated_at)) }}
+          </div>
+        </div>
       </div>
       {!! $article->body !!}
     </div>
