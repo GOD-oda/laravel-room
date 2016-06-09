40a1e77e6fd4b01352a699c6ef99dec9fe728b89
oda <oda_takahiro@mut.co.jp>
2016-06-09 18:17:37 +0900

    ちょっと修正

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,31 +16,4 @@ Route::group(['middleware' => 'web'], function () {
     // ブログ
     Route::get('/', 'ArticlesController@index');
     Route::get('/{entry}', 'ArticlesController@show');
-
-
-/*
-    // ブログ
-    Route::group(['domain' => 'blog.t-oda.tech'], function () {
-        Route::auth();
-        Route::group(['middleware' => 'auth'], function () {
-            Route::group(['prefix' => 'admin'], function () {
-                Route::post('blog/search', 'Admin\BlogController@search');
-                Route::resource('blog', 'Admin\BlogController');
-                Route::post('payment/search', 'PaymentController@search');
-                Route::resource('payment', 'PaymentController');
-                Route::get('/', function () {
-                    return redirect('admin/blog');
-                });
-            });
-        });
-        Route::resource('/article', 'ArticlesController', ['only' => ['index', 'show']]);
-        Route::get('/', function () {
-            return redirect()->action('ArticlesController@index');
-        });
-    });
-    // ポートフォリオ
-    Route::get('/', function () {
-        return 'ポートフォリオ製作中';
-    });
-*/
 });
