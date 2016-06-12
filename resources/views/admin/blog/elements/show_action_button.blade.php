90a7c56f70036237c4742d197391b189b8b5999c
Takahiro Oda <conpw3@gmail.com>
2016-06-12 16:25:20 +0900

    iconを修正

--- resources/views/admin/blog/elements/show_action_button.blade.php
+++ resources/views/admin/blog/elements/show_action_button.blade.php
@@ -3,6 +3,7 @@
     <a class="btn-floating btn-large red"><i class="material-icons">menu</i></a>
     <ul>
       <li><a href="{{ action('Auth\AuthController@logout') }}" class="btn-floating waves-effect waves-light red"><i class="material-icons prefix">exit_to_app</i></a></li>
+      <li><a href="{{ action('Admin\BlogController@edit', $article->uri) }}" class="btn-floating waves-effect waves-light red"><i class="material-icons prefix">change_history</i></a></li>
       <li><a href="{{ url('admin/blog/create') }}" class="btn-floating waves-effect waves-light red"><i class="material-icons prefix">create</i></a></li>
     </ul>
   </div>
