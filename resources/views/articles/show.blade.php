ddbf02d259d128909b7371212c9843eea2867e51
oda <oda_takahiro@mut.co.jp>
2016-06-15 10:20:37 +0900

    snsボタン設置できたらいいなー

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.home_master')
 
 @section('content')
-{{-- @include('elements.tabs') --}}
-
 <div class="container">
   <div class="section">
     <div class="article-show">
@@ -18,6 +16,28 @@
       </div>
       {!! $article->body !!}
     </div>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <a class="twitter-share-button" href="https://twitter.com/share" data-dnt="true">Tweet</a>
+    <div class="fb-like" data-href="{{ url($article->uri) }}" data-layout="button_count" data-action="like" data-show-faces="true"></div>
+    <a href="http://b.hatena.ne.jp/entry/{{ url($article->uri) }}" class="hatena-bookmark-button" data-hatena-bookmark-title="laravel-room" data-hatena-bookmark-layout="standard-balloon" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>
+    <script>
+      window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));
+    </script>
+    <div id="fb-root"></div>
+    <script>
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.0";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+    <script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async">
+      {lang: "ja"}
+    </script>
   </div>
 </div>
 @stop
