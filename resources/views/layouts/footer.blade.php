744d7dfd616fbad34f2d2d93b7e4758b738cca87
oda <oda_takahiro@mut.co.jp>
2016-06-28 19:39:45 +0900

    fbいいねボタンと自己紹介を追加

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -11,13 +11,16 @@
       </div>
       <div class="col l4 offset-12 s12">
         <h5 class="white-text">
-          author
+          AUTHOR
         </h5>
-        <div>
+        <p class="grey-text text-lighten-4">
+          {{ Config::get('laravel-room.author') }}
+        </p>
           <a href="https://twitter.com/share" class="twitter-share-button" data-via="Tkahiro_Oda" data-size="large">
             Tweet
           </a>
-        </div>
+          <div class="fb-like" data-href="http://laravel-room.com/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+
         <!-- <div class="chip">
           <img src="{{ asset('img/laravel5.jpg') }}" alt="">
         </div> -->
--- resources/views/layouts/home_master.blade.php
+++ resources/views/layouts/home_master.blade.php
@@ -53,5 +53,13 @@
   <script src="{{ asset('js/boot_materialize.js') }}"></script>
   <script src="{{ asset('js/article.js') }}"></script>
   <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.6&appId=1399019850124984";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 </body>
 </html>
