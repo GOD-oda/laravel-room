b14e9ff94cf08bc5d007bd0e358ce52ed7dd32c3
Takahiro_Oda <conpw3@gmail.com>
2016-10-02 15:29:41 +0900

    記事詳細のリストを調整

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.home_master')
 
 @section('content')
-<div class="article-show">
-  <div class="container">
-    <div class="section">
-      <h1>{{ $article->title }}</h1>
-      <div class="divider"></div>
-      <div class="row">
-        <div class="col s12">
-          <div class="right mt5">
-            <i class="material-icons">build</i> {{ date('Y.m.d', strtotime($article->created_at)) }}
-            <i class="material-icons">autorenew</i> {{ date('Y.m.d', strtotime($article->updated_at)) }}
+<article>
+  <div class="article-show">
+    <div class="container">
+      <section class="section">
+        <h1>{{ $article->title }}</h1>
+        <div class="divider"></div>
+        <div class="row">
+          <div class="col s12">
+            <div class="right mt5">
+              <i class="material-icons">build</i> {{ date('Y.m.d', strtotime($article->created_at)) }}
+              <i class="material-icons">autorenew</i> {{ date('Y.m.d', strtotime($article->updated_at)) }}
+            </div>
           </div>
         </div>
-      </div>
-      <div class="article-body">
-        {!! $article->body !!}
-      </div>
+        <div class="article-body">
+          {!! $article->body !!}
+        </div>
+      </section>
     </div>
   </div>
-</div>
+</article>
 @stop
