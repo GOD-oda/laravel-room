72ec1a3acad4fec3d1c00ec8b01f9a4bda61632f
Takahiro_Oda <conpw3@gmail.com>
2016-11-17 00:09:38 +0900

    update sideNav and footer

--- config/laravel-room.php
+++ config/laravel-room.php
@@ -11,11 +11,6 @@ return [
         'discription' => 'このサイトは、日々奮闘する下っ端プログラマのt-odaがLaravelの情報を提供し、運営するサイトです。',
     ],
 
-    // parallax text
-    'parallax' => [
-        'title' => 'Laravel Room'
-    ],
-
     // FOOTER
     'copyright' => 'Copyright © 2016 Laravel Room All rights reserved.',
     'content' => 'このサイトは、Laravelに関する情報の備忘録や周知を目的を目的としています。また、このサイトはLaravel5.2とMaterializeで構築されています。',
--- resources/views/elements/navbar.blade.php
+++ resources/views/elements/navbar.blade.php
@@ -1,28 +1,10 @@
 <div class="navbar-fixed">
   <nav>
-    <div class="nav-wrapper teal lighten-1">
+    <div class="nav-wrapper teal">
       <!-- trigger side navigation -->
       <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">view_headline</i></a>
-      <!-- side navigation -->
-      <ul id="slide-out" class="side-nav">
-        <li>
-          <div class="userView teal lighten-2">
-            <img src="{{ asset('img/profile-background.jpeg') }}" alt="" class="circle">
-            <span class="blue-grey-text text-lighten-5 name">t-oda</span>
-            <a href="#"><span class="blue-grey-text text-lighten-5 email">takahiro.tech.oda@gmail.com</span></a>
-            <ul class="sns-logo">
-              <li><a href="https://twitter.com/Tkahiro_Oda" target="_blank"><i class="fa fa-twitter-square"></i></a></li>
-            </ul>
-          </div>
-        </li>
-        <li>
-          <a href="{{ url('/') }}" class="black-text"><i class="material-icons left">home</i>TOP</a>
-        </li>
-        <li>
-          <a href="{{ action('ContactController@index') }}" class="black-text"><i class="material-icons left">info_inline</i>お問い合わせ</a>
-        </li>
-      </ul>
-      <a href="{{ url('/') }}" class="brand-logo">{{ Config::get('laravel-room.parallax.title') }}</a>
+      <a href="{{ url('/') }}" class="brand-logo">{{ Config::get('laravel-room.title') }}</a>
+
       <!-- TODO
         検索の処理を追加するまでは出さない。
         Enterで動くようにすること
@@ -38,3 +20,20 @@
     </div>
   </nav>
 </div>
+<!-- side navigation -->
+<ul id="slide-out" class="side-nav">
+  <li>
+    <div class="userView teal lighten-2">
+      <img src="{{ asset('img/profile-background.jpeg') }}" alt="" class="circle">
+      <span class="name">t-oda</span>
+      <a href="#"><span class="email">takahiro.tech.oda@gmail.com</span></a>
+      <a href="https://twitter.com/Tkahiro_Oda" target="_blank"><i class="fa fa-twitter-square"></i></a>
+    </div>
+  </li>
+  <li>
+    <a href="{{ url('/') }}" class="black-text waves-effect waves-teal waves-light waves-ripple"><i class="material-icons left">home</i>TOP</a>
+  </li>
+  <li>
+    <a href="{{ action('ContactController@index') }}" class="black-text waves-effect waves-teal waves-light waves-ripple"><i class="material-icons left">info_inline</i>お問い合わせ</a>
+  </li>
+</ul>
