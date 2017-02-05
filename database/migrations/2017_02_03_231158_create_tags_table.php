c4a0873579afd4643ce5d794f87e70af7f6305b3
t.oda <takahiro.tech.oda@gmail.com>
2017-02-05 22:00:10 +0900

    記事のタグ用マイグレーションファイルを追加

---
+++ database/migrations/2017_02_03_231158_create_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('article_id')->unsigned();
+            $table->string('tag', 255);
+            $table->timestamps();
+            $table->softDeletes();
+
+            // 外部キーの設定
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
