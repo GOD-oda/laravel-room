26e2fb3bfc992000c857019b1289689fa7c951a7
t.oda <takahiro.tech.oda@gmail.com>
2017-02-06 17:28:50 +0900

    タグのテーブル修正
    ・カラム名の変更
    ・論理削除は使わない方向で

--- database/migrations/2017_02_03_231158_create_tags_table.php
+++ database/migrations/2017_02_03_231158_create_tags_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreateTagsTable extends Migration
 {
@@ -17,9 +16,8 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('article_id')->unsigned();
-            $table->string('tag', 255);
+            $table->string('tag_name', 255);
             $table->timestamps();
-            $table->softDeletes();
 
             // 外部キーの設定
             $table->foreign('article_id')->references('id')->on('articles');
