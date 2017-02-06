8f3312ec7c44faf7d689d896bee4b391c17a9979
t.oda <takahiro.tech.oda@gmail.com>
2017-02-06 18:57:08 +0900

    ajaxを利用するときのcsrfトークンなど

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
-  $('.delete-btn').on('click', function() {
-    if (confirm("本当の削除しますか？")) {
-      submit();
-    } else {
-      return false;
-    }
-  });
-  $('.alert').delay(3000).slideUp(300);
+    // 記事削除
+    $('.delete-btn').on('click', function() {
+        if (confirm("本当に削除しますか？")) {
+            submit();
+        } else {
+            return false;
+        }
+    });
+
+    // アラートはスライドさせて非表示にする
+    $('.alert').delay(3000).slideUp(300);
+
+    // タグ削除
+    $('.delete-tag').on('click', function() {
+        var article_id = $(this).data('article_id');
+        var tag_name = $(this).data('tag_name');
+        var target_url = $(this).data('url');
+
+        var data = {
+            article_id: article_id,
+            tag_name: tag_name
+        }
+
+        $.post(target_url, data, function(response) {
+            console.log(response);
+        });
+    });
+
 });
--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -9,6 +9,8 @@
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>{{ Config::get('word.title') }}</title>
 
   <!-- Compiled and minified CSS -->
@@ -56,6 +58,13 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.5/marked.min.js"></script>
 
   <!-- Custome script for this template -->
+  <script>
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      }
+    });
+  </script>
   <script src="{{ asset('js/materialize.min.js') }}"></script>
   <script src="{{ asset('js/boot_materialize.js') }}"></script>
   <script src="{{ asset('js/boot_marked.js') }}"></script>
