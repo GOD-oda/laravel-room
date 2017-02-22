53ff6275dbabb9c5259405717b49038f9f12bf3d
t.oda <takahiro.tech.oda@gmail.com>
2017-02-22 23:25:41 +0900

    fix path from http to https

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -7,7 +7,7 @@
 <meta name="twitter:domain" content="laravel-room.com" />
 <meta name="twitter:title" content="{{ $article->title }}" />
 <meta name="twitter:description" content="{{ $article->discription }}" />
-<meta name="twitter:image" content="{{ is_null($article->image_path) ? '' : asset('img/thumbnail/'.$article->image_path) }}" />
+<meta name="twitter:image" content="{{ is_null($article->image_path) ? '' : secure_asset('img/thumbnail/'.$article->image_path) }}" />
 @endsection
 
 @section('facebook-tags')
@@ -15,14 +15,14 @@
 <meta property="og:type" content="website" />
 <meta property="og:title" content="{{ $article->title }}" />
 <meta property="og:description" content="{{ $article->discription }}" />
-<meta property="og:image" content="{{ is_null($article->image_path) ? '' : asset('img/thumbnail/'.$article->image_path) }}" />
+<meta property="og:image" content="{{ is_null($article->image_path) ? '' : secure_asset('img/thumbnail/'.$article->image_path) }}" />
 @endsection
 
 @section('content')
 <article>
   <div class="container">
     <div class="post-image">
-      <img src="{{ asset('img/thumbnail/'.$article->image_path) }}" alt="">
+      <img src="{{ secure_asset('img/thumbnail/'.$article->image_path) }}" alt="">
     </div>
     <section class="article-box card">
       <div class="row">
@@ -42,10 +42,10 @@
         <div class="col m1 offset-m1 hide-on-small-only sns-area center-align">
           <div class="sns-label">share</div>
           <a href="https://twitter.com/share" class="sns-btn" data-url="{{ $article->uri }}" data-via="Tkahiro_Oda">
-            <img src="{{ asset('img/sns/twitter.svg') }}" alt="ツイッターアイコン" class="sns-icon">
+            <img src="{{ secure_asset('img/sns/twitter.svg') }}" alt="ツイッターアイコン" class="sns-icon">
           </a>
           <a href="https://www.facebook.com/sharer/sharer.php?u=http://laravel-room.com/{{ $article->uri }}" target="_blank" rel="nofollow">
-            <img src="{{ asset('img/sns/facebook.svg') }}" alt="" class="sns-icon">
+            <img src="{{ secure_asset('img/sns/facebook.svg') }}" alt="" class="sns-icon">
           </a>
         </div>
       </div>
