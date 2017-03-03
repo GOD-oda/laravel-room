78aa0d2773a4d49255ca6950fa1be98fa35757a4
t.oda <takahiro.tech.oda@gmail.com>
2017-03-03 22:48:40 +0900

    remove comment

--- resources/views/layouts/home_master.blade.php
+++ resources/views/layouts/home_master.blade.php
@@ -13,43 +13,24 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="{{ Config::get('laravel-room.meta.discription') }}">
   <meta name="author" content="{{ Config::get('laravel-room.meta.author') }}">
-
-  <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
   <title>{{ Config::get('laravel-room.title') }}</title>
-
   <link rel="shortcut icon" href="{{ asset('img/laravel.ico') }}" />
-
-  <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
-
-  <!-- Fonts from Google Fonts -->
   @include('elements.fonts')
-
-  <!--Import Google Icon Font-->
   <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!-- Font Awesome -->
   <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-
   <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
-
-  <!-- Custom styles for this template -->
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
   <![endif]-->
 
-  <!-- twitter tags -->
   @yield('twitter-tags')
-
-  <!-- facebook tags -->
   @yield('facebook-tags')
 
-  <!-- Google Analytics Tracking Code -->
   <script src="{{ asset('js/trackingcode.js') }}"></script>
 </head>
 <body class="blue-grey lighten-5">
@@ -66,7 +47,6 @@
 
   @include('layouts.footer')
 
-  <!-- Bootstrap core JavaScript -->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
   <script src="{{ asset('js/boot_materialize.js') }}"></script>
